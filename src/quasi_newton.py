cb0e84585f70575419778e1bd2b48e0473e23459
fullswing <shibukawa_ryosuke_18@stu-cbms.k.u-tokyo.ac.jp>
2019-03-14 21:57:10 +0900

    generalize algorithm for any shape of matrix

--- src/quasi_newton.py
+++ src/quasi_newton.py
@@ -5,52 +5,72 @@ import numpy as np
 import copy, time
 import matplotlib.pyplot as plt
 import queue
+from numpy import genfromtxt
 from collections import deque
+from scipy.optimize import line_search
+from sk_balancing import sinkhorn
+
 
 def preprocess(A):
     A = np.array(A)
-    n, _ = A.shape
-    #print(A)
+    n, m = A.shape
+    if n == m:
+        A += 1e-5 * np.eye(n)
     delete_row_index = []
     for i in range(n):
         if len(A[i]) > 0 and np.count_nonzero(A[i]) <= 1:
             delete_row_index.append(i)
     A = np.delete(A, delete_row_index, 0)
-    #print(A)
     print("deleted row:", np.array(delete_row_index))
     delete_col_index = []
     _, m = A.shape
     for i in range(m):
-        #print(np.count_nonzero(A[:,i]))
         if len(A[:,i]) > 0 and np.count_nonzero(A[:,i]) <= 1:
             delete_col_index.append(i)
     A = np.delete(A, delete_col_index, 1)
-    #print(A)
     print("deleted col:", np.array(delete_col_index))
     print(A.shape)
+    #A = sinkhorn(A)
     return A
+
+def objective_function(x, A):
+    n, m = A.shape
+    row_scale = np.array(x[0:n])
+    col_scale = np.array(x[n:])
+    #print(row_scale, col_scale)
+    return np.exp(row_scale).dot(A).dot(np.exp(col_scale)) - sum(row_scale) - sum(col_scale)
 
 def gradient(x, A):
     g = []
-    n = len(x)
+    row, col = A.shape
+    scale_row = np.array(x[0:row])
+    assert len(scale_row) == row
+    scale_col = np.array(x[row:])
+    assert len(scale_col) == col
     hist = []
-    for i in range(n):
+    for i in range(row):
         var = 0
-        for j in range(n):
-            #print(A[i][j] * np.exp(x[j]))
-            var += A[i][j] * np.exp(x[j])
-            #if i == j:
-            #    var += A[i][j] * np.exp(x[j])
+        for j in range(col):
+            var += A[i][j] * np.exp(scale_col[j])
             if var == float('inf'):
-                print(A[i][j], np.exp(x[j]), x[j], j)
+                print(A[i][j], np.exp(scale_col[j]), scale_col[j], j)
             assert var != float('inf')
-            hist.append(np.exp(x[j]))
-        var *= np.exp(x[i])
+            #hist.append(np.exp(scale_col[j]))
+        var *= np.exp(scale_row[i])
         var -= 1
         g.append(var)
-    #print("gradient:{}".format(np.array(g)))
-    if g[0] == float('inf'):
-        print(hist)
+    for j in range(col):
+        var = 0
+        for i in range(row):
+            var += A[i][j] * np.exp(scale_row[i])
+            if var == float('inf'):
+                print(A[i][j], np.exp(scale_row[i]), scale_row[i], i)
+            assert var != float('inf')
+            #hist.append(np.exp(scale_row[i]))
+        var *= np.exp(scale_col[j])
+        var -= 1
+        g.append(var)
+    #print(x[0:row] - x[row:])
     return np.array(g)
 
 def two_loop_recursion(grad, s, y):
@@ -60,80 +80,66 @@ def two_loop_recursion(grad, s, y):
         return 0.01 * d
     a = []
     hist = []
-    #for i in range(n):
     for i in reversed(range(n)):
         alpha = s[i].dot(d) / s[i].dot(y[i])
         v = s[i].dot(y[i])
         hist.append(v)
-        #print("alpha:", alpha)
         d = np.array(d) - alpha * y[i]
         a.append(alpha)
-        #print(len(s))
-        #print(len(y))
-    #d = s[n-1].dot(y[n-1]) / y[n-1].dot(y[n-1]) * d
-    #print("corr:",s[-1].dot(y[-1]) / y[-1].dot(y[-1]))
-    #print("bunbo:",y[-1].dot(y[-1])) 
-    #print("bunshi:",s[-1].dot(y[-1]))
+
     d = (s[n-1].dot(y[n-1]) / y[n-1].dot(y[n-1])) * d
-    #print(a)
-    #print(hist)
     a = a[::-1]
-    #print("hoge search vec:", d)
-    #for i in reversed(range(n)):
     
     for i in range(n):
-        #print(d)
         b = y[i].dot(d) / s[i].dot(y[i])
-        #print(s[i])
-        #print(a[i])
         d = d + s[i] * (a[i] - b)
-        #print("search vec:", d, a[i], b)
     return d
 
-def l_bfgs(x, A, m=10, e=1e-6, max_iter=100):
+def l_bfgs(x, A, m=10, e=1e-6, max_iter=100, prefix='hic'):
     k = 0
     s = deque()
     y = deque()
     grad = gradient(x, A)
+    row, col = A.shape
+    assert len(grad) == len(A) + len(A[0])
     l = []
     norm_hist = []
     lr = 0.1
     init = 0.1
     min_lr = 0.0001
     min_loss = 1e+10
-    f1 = open('../result/hic_loss_with_annealing.txt', 'w')
-    f2 = open('../result/lr_history_for_hic.txt', 'w')
-    while np.linalg.norm(grad) > e or k <= max_iter:
+    f1 = open('../result/'+prefix+'_loss_with_annealing.txt', 'w')
+    f2 = open('../result/'+prefix+'lr_history_for_hic.txt', 'w')
+    while np.linalg.norm(grad) > e and k <= max_iter:
         d = two_loop_recursion(grad, s, y)
-        #print("search vector:", d)
         tmp = np.linalg.norm(grad)
         f1.write(str(tmp) + '\n')
-        x = np.array(x) + lr * np.array(d)
-        lr = max(lr*0.8, min_lr)
+        #print(x)
+        gfk = gradient(x, A)
+        #alpha, fc, gc, new_fval, old_fval, new_slope = line_search(f=objective_function,myfprime=gradient,gfk=gfk,args=(A,),xk=x,pk=d,amax=1)
+        alpha = None
+        if alpha == None:
+            x = np.array(x) + min(lr*0.8, min_lr) * np.array(d)
+        else:
+            print("alpha:", alpha)
+            x = np.array(x) + alpha * np.array(d)
+        #lr = max(lr*0.8, min_lr)
         f2.write(str(lr) + '\n')
         
-        #plt.hist(d)
-        #plt.savefig("../result/" + str(k).zfill(2) + ".png")
-        #plt.clf()
-        loss = np.linalg.norm(np.diag(np.exp(x)).dot(A).dot(np.exp(x))-np.ones(len(x)))
+        loss = np.linalg.norm(grad)
+        """
         if loss > 3 * min_loss:
             lr = init
         else:
             min_loss = loss
-        #if len(l) > 0 and loss < l[-1]:
-            #print(l[-1])
-            #print(loss, l[-1])
+        """
         l.append(loss)
         norm_hist.append(np.linalg.norm(grad))
-        #print("step:",k)
         if k % 5 == 0:
-            print("norm:", np.linalg.norm(grad))        
-            #print("loss:", loss)
-        #print("new x:", x)
-        #print("result",np.diag(np.exp(x)).dot(A).dot(np.exp(x)))
+            print("step:{} norm:{}".format(k, np.linalg.norm(grad)))
+
         newGrad = gradient(x, A)
         if len(s) == m:
-            #print("hoge")
             s.popleft()
             y.popleft()
         s.append(d)
@@ -146,7 +152,9 @@ def l_bfgs(x, A, m=10, e=1e-6, max_iter=100):
     f1.close()
     f2.close()
     print(np.exp(x))
-    print(np.diag(np.exp(x)).dot(A).dot(np.diag(np.exp(x))))
+    row_scale = x[0:row]
+    col_scale = x[row:]    
+    print(np.diag(np.exp(row_scale)).dot(A).dot(np.diag(np.exp(col_scale)) - np.eye(row)))
     #print(k)
     return x, l
 
@@ -156,22 +164,25 @@ if __name__ == '__main__':
     trg = preprocess(trg)
     #trg = np.array([[1.2,0.4],[0.4, 1.2]], float)
     #trg = np.array([[100,100002,3],[100002,1000,0.9],[3,0.9,5]], float)
-    #trg = np.array([[10,1.2,3],[1.2,7,0.9],[3,10,0.9]], float)
+    #trg = np.array([[100,12,3],[15, 8, 9],[3,100,1]], float)
+    #trg = np.array([[10,1.2,3],[1.2,7,0.9],[3,0.9, 10]], float)
+    #trg = np.array([[10,1.2,3],[1.2,7,0.9]], float)
+    #trg = genfromtxt("../data/hessenberg20.txt", delimiter=',')
     #trg = np.array([[1,2],[2,4]], float)
-    trg += 1e-3
+    #trg += 1e-3
     #trg = trg / 100
 
     #trg = trg
     #print(trg)
     n, m = trg.shape
-    x = -np.ones(n)
+    x = -np.ones(n+m)
     #x = np.zeros(n)
     #x = np.array([0, 0])
     start = time.time()
-    x, l = l_bfgs(x, trg)
+    x, l = l_bfgs(x, trg,prefix='k562_hic')
     end = time.time()
     print("elapsed time:{} sec.".format(end-start))
     plt.plot(l)
     plt.yscale('log')
     plt.xticks(list(range(0,len(l), 5)))
-    plt.savefig("../mat_newton_loss.png")
+    plt.savefig("../k562_newton_loss.png")
